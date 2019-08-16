34b88f4c1df10cb6d98c38274c666108671007fd
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 15:43:24 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,8 +11,8 @@ pipeline {
                 
                 yum install -y docker git
                 service docker start
-                docker stop $(docker ps -q)
-                docker rm $(docker ps -q -a)
+                docker stop \$(docker ps -q)
+                docker rm \$(docker ps -q -a)
                 git clone -b master https://idonoga:${GIT_PASSWORD}@github.com/idonoga/simpleflask-docker-v1.git .
                 """
                 
