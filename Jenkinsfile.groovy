e4d6bd566e07efdfc882e1a1d9aa56f6abe65c8d
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 15:16:42 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
                 cleanWs()
                 echo "Building flask container"
                 sh """
-                git clone -b master https://idonoga:${GIT_PASSWORD}@github.com/idonoga/simpleflask-docker-v1.git
+                git clone -b master https://idonoga:${GIT_PASSWORD}@github.com/idonoga/simpleflask-docker-v1.git .
                 """
                 
             }
