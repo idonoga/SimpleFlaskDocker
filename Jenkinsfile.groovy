2a1732994649c27c2b484c6f6cced877c923c0d9
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 18:28:55 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,8 +15,6 @@ pipeline {
                 cleanWs()
                 sh """
                 git clone -b master https://idonoga:${GIT_PASSWORD}@github.com/idonoga/simpleflask-docker-v1.git .
-                docker rm flaskcontainer
-                docker rm ngnixproxy
                 """
                 
             }
