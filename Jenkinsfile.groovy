c7cb1a194717beb0cef07df82a9e15d3f89e9f82
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 18:26:47 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -54,7 +54,10 @@ pipeline {
                 """
                 script{
                 def RESPONSE = sh(script: "curl -s --head  --request GET http://localhost | grep '200 OK'", returnStdout: true)
-                echo "${RESPONSE}"
+                if (${RESPONSE} != 'HTTP/1.1 200 OK')
+                    {
+                        sh "exit 1"
+                    }
                 }
                 
                 
