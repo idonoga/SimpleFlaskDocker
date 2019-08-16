0a088545a097ca58a94ff7da24aa2d290bec637b
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 16:59:02 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipeline {
                echo "Building flask container"
                sh """
                 docker build -t flask:latest ./mydockerflask/
-                IMAGE_ID=\\$\\(docker images | grep latest | awk '\\{print %243\\}'\\)
+                IMAGE_ID=\\%24\\(docker images | grep latest | awk '\\{print %243\\}'\\)
                 docker tag %24IMAGE_ID idonoga/flask:firsttry
                 docker push idonoga/flask
                 """
