cfd694c1c1ae3c777a1cdca284cece8ddaaea64d
idonoga <53582447+idonoga@users.noreply.github.com>
2019-08-16 18:04:20 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -50,7 +50,7 @@ pipeline {
             sh """
                 docker run -v /var/run/docker.sock:/var/run/docker.sock flask:latest
                 """
-                def response = sh(script: 'curl -s --head  --request GET http://localhost | grep '200 OK\'', returnStdout: true)
+                def response = sh(script: "curl -s --head  --request GET http://localhost | grep '200 OK'", returnStdout: true)
                 if(response != 'HTTP/1.1 200 ok')
                 {
                     exit 1
